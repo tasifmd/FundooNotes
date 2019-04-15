dfb79e06ef9002e680cd4dfde95d49e492a2d18b
tasifmd <tasifmd96@gmail.com>
2019-04-15 13:20:04 +0530

    label done

--- src/main/java/com/bridgelabz/fundoo/config/RabbitMqConfig.java
+++ src/main/java/com/bridgelabz/fundoo/config/RabbitMqConfig.java
@@ -12,7 +12,6 @@ import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
 
 /**
  * Purpose : RabbitMq Configuration class
