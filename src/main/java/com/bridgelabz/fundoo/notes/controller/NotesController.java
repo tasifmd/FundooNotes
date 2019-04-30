df9d7c65d9cb39d03af755c22be6c9b13f4ebe80
tasifmd <tasifmd96@gmail.com>
2019-04-30 17:13:52 +0530

    label

--- src/main/java/com/bridgelabz/fundoo/notes/controller/NotesController.java
+++ src/main/java/com/bridgelabz/fundoo/notes/controller/NotesController.java
@@ -2,8 +2,6 @@ package com.bridgelabz.fundoo.notes.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
